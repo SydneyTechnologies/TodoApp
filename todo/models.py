c04ef793e17d88f6288a6fc7beb2a997be4c24ad
sydney <sydney.idundun@gmail.com>
2022-08-19 19:58:09 +0200

    created the Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
+from ast import Num
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
+    due_date = models.DateTimeField()
+    def __str__(self) -> str:
+        return self.title
